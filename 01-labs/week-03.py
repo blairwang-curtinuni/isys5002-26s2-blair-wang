371f97cfb1884e5fabec954c54d70c7605c3163c
blairwang-curtinuni <blair.wang@curtin.edu.au>
2026-08-03 02:29:57 +0000

    [human-only] [feat] Very basic work for W3 lab

---
+++ 01-labs/week-03.py
@@ -0,0 +1,8 @@
+# Activity 1
+
+# Displaying the weather menu options
+print("Weather Menu:")
+print("1. Check Temperature")
+print("2. Check Humidity")
+print("3. Check Wind Speed")
+print("4. Exit")
